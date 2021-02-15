1dd04565dc977e47dd98bd2ee43e033f81d8cbb3
Morgan <morgantosini@gmail.com>
2021-02-15 16:15:42 -0500

    working on route for signing in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,9 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
+  def signin
+  end
+
   # GET /users/1 or /users/1.json
   def show
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root "users#index"
   resources :events
-  resources :users
+  # resources :users
+  resources :users do
+    get "signin", on: :collection
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
